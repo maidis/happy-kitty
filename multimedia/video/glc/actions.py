7756630c3abb0ee35d72bce859224d5bfbca16f2
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-03-27 22:14:08 +0000

    add multimedia component and glc

---
+++ multimedia/video/glc/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "%s" % get.srcNAME()
+
+def setup():
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=release", installPrefix="/usr")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dobin("scripts/capture.sh")
+    pisitools.dobin("scripts/encode.sh")
+    pisitools.dobin("scripts/play.sh")
+    pisitools.dodoc("CHANGELOG")
