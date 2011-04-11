98e519b5f7c4bb7912793c341d3e09f2e9cbc2a2
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-04-11 16:31:10 +0000

    add violetland

---
+++ trunk/game/action/violetland/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "violetland-v%s" % get.srcVERSION()
+
+def setup():
+    cmaketools.configure()
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dosym("/usr/share/violetland/icon-light.png", "/usr/share/pixmaps/violetland.png")
+
+    pisitools.dodoc("CHANGELOG", "README*")
