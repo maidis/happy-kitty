8cbd1252d2a8badf00a18fbef218aa9223ce9b7d
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-04-14 17:50:10 +0000

    add paged geometry add-on for ogre

---
+++ library/ogre-pagedgeometry/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "ogre-pagedgeometry"
+
+def setup():
+    cmaketools.configure("-DPAGEDGEOMETRY_BUILD_SAMPLES=0")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("GettingStarted.txt", "Todo.txt", "docs/*.odt")
