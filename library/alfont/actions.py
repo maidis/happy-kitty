1cd782fdc1f1e173d0d690ee95e7f94a18e0f8ca
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-04-17 19:36:00 +0000

    add alfont

---
+++ library/alfont/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.dolib_a("lib/linux/libalfont.a")
+
+    pisitools.insinto("/usr/include", "include/alfont*.h")
+
+    pisitools.dodoc("CHANGES.txt", "README.txt", "docs/*")
