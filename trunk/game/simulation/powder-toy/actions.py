648ba1924bb40a84dea12d6e290cacba8a7845b6
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-04-23 12:35:41 +0000

    add powder toy, please test on a 64 bit machine and tell me the is it works or not

---
+++ trunk/game/simulation/powder-toy/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "FacialTurd-The-Powder-Toy-194af75"
+
+def build():
+    autotools.make("powder")
+
+def install():
+    pisitools.dobin("build/powder")
+
+    pisitools.dodoc("console_README", "LICENSE", "README", "roadmap")
