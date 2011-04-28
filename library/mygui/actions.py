a025a40f070de3133ee3255409d283654d02699c
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-04-28 13:10:51 +0000

    mygui: i'm trying to find problem

--- library/mygui/actions.py
+++ library/mygui/actions.py
@@ -14,14 +14,19 @@ def setup():
     cmaketools.configure("-DCMAKE_BUILD_TYPE=release \
                           -DMYGUI_BUILD_SAMPLES:BOOL=OFF \
                           -DMYGUI_INSTALL_SAMPLES:BOOL=OFF \
-                          -DMYGUI_BUILD_TOOLS:BOOL=OFF \
-                          -DMYGUI_BUILD_PLUGINS:BOOL=OFF", installPrefix="/usr")
+                          -DMYGUI_INSTALL_SAMPLES_SOURCE=ON \
+                          -DMYGUI_BUILD_TOOLS:BOOL=ON \
+                          -DMYGUI_INSTALL_TOOLS:BOOL=ON \
+                          -DMYGUI_INSTALL_MEDIA:BOOL=ON \
+                          -DMYGUI_BUILD_PLUGINS:BOOL=ON \
+                          -DMYGUI_INSTALL_DOCS:BOOL=ON", installPrefix="/usr")
 
 def build():
     cmaketools.make()
 
 def install():
     # libMyGUI.OgrePlatform.a ?
+    # non-bin files in bin: plugins.cfg, resources.xml? opt?
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("ChangeLog.txt", "COPYING.LESSER", "Readme.txt")
