3e8db02075b056caeb21071e3bae8d741b5c79d9
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-05-02 22:50:06 +0000

    add chickennix

---
+++ game/action/chickennix/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.doman("docs/chickennix.1")
+
+    pisitools.dodoc("changelog", "README", "docs/AUTHORS", "docs/LICENSE", "docs/VERSION")
