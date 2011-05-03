a804880de2bbbf19ab6ed14ebe2d8906210ca59c
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-05-03 19:39:29 +0000

    add open surge

---
+++ game/arcade/opensurge/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "opensurge-src-build563"
+
+def setup():
+    shelltools.system("./configure")
+    #cmaketools.configure()
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dobin("opensurge")
+    pisitools.insinto("/usr/share/opensurge/characters", "characters/*")
+    pisitools.remove("/usr/share/opensurge/opensurge")
+    pisitools.remove("/usr/share/opensurge/readme.html")
+    pisitools.remove("/usr/share/opensurge/license.txt")
+
+    pisitools.dodoc("readme.html", "bleeding_edge.txt", "license.txt")
