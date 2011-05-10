4289e23a2993ffe614f30b94bf032ce5b78c6ff2
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-05-10 18:35:15 +0000

    add klikit

---
+++ trunk/game/puzzle/klikit/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import qt4
+from pisi.actionsapi import get
+
+WorkDir = "Klikit-%s" % get.srcVERSION()
+
+def setup():
+    qt4.configure()
+
+def build():
+    qt4.make()
+
+def install():
+    qt4.install()
+
+    pisitools.dobin("Klikit")
+
+    pisitools.insinto("/usr/share/pixmaps", "images/board.png", "klikit.png")
+
+    pisitools.dodoc("*.txt")
+    pisitools.dohtml("doc/*")
