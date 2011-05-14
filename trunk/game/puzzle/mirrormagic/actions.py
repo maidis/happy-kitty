fa371941f6649fc4b64810cf76da4351ae1e5d1e
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-05-14 11:37:13 +0000

    add mirror magic

---
+++ trunk/game/puzzle/mirrormagic/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-·
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def setup():
+    pisitools.dosed("Makefile", "# RO_GAME_DIR = /usr/games", "RO_GAME_DIR = /usr/share/mirrormagic")
+    pisitools.dosed("Makefile", "# RW_GAME_DIR = /var/games", "RW_GAME_DIR = /var/games")
+    pisitools.dosed("Makefile", "TARGET=x11", "TARGET=sdl")
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.dobin("mirrormagic")
+
+    pisitools.insinto("/usr/share/mirrormagic/graphics", "graphics/*")
+    pisitools.insinto("/usr/share/mirrormagic/levels", "levels/*")
+    pisitools.insinto("/usr/share/mirrormagic/music", "music/*")
+    pisitools.insinto("/usr/share/mirrormagic/sounds", "sounds/*")
+
+    pisitools.dodoc("CHANGES", "COPYING", "CREDITS", "mirrormagic-2.0.2.lsm", "README", "TODO")
