85a8f83e58e3be25c094ba1247f8975ebee02021
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-06-18 19:51:09 +0000

    add symbolmachine

---
+++ game/puzzle/symbolmachine/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import qt4
+
+WorkDir = "."
+
+def setup():
+    qt4.configure()
+
+def build():
+    qt4.make()
+
+def install():
+    qt4.install()
+
+    pisitools.dobin("symbolmachine")
+
+    pisitools.insinto("/usr/share/pixmaps", "symbolmachine.png")
+
+    pisitools.dodoc("AUTHORS", "COPYING", "README")
