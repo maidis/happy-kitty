58954a6d27caad2c74ebc03af8c6ff4cda0e11be
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-06-19 11:05:48 +0000

    atanks: version bump

--- game/arcade/atanks/actions.py
+++ game/arcade/atanks/actions.py
@@ -13,22 +13,8 @@ def build():
                     INSTALLDIR=/usr/share/atanks")
 
 def install():
-    pisitools.dobin("atanks")
+    autotools.rawInstall("PREFIX=%s/usr/ INSTALL=\"install -c\"" % get.installDIR())
 
-    pisitools.insinto("/usr/share/atanks/button", "button/*")
-    pisitools.insinto("/usr/share/atanks/misc", "misc/*")
-    pisitools.insinto("/usr/share/atanks/missile", "missile/*")
-    pisitools.insinto("/usr/share/atanks/sound", "sound/*")
-    pisitools.insinto("/usr/share/atanks/stock", "stock/*")
-    pisitools.insinto("/usr/share/atanks/tank", "tank/*")
-    pisitools.insinto("/usr/share/atanks/tankgun", "tankgun/*")
-    pisitools.insinto("/usr/share/atanks/text", "text/*")
-    pisitools.insinto("/usr/share/atanks/title", "title/*")
-    pisitools.insinto("/usr/share/atanks", "unicode.dat")
-    pisitools.insinto("/usr/share/atanks", "credits.txt")
-
-    #pisitools.dodesktop("atanks.desktop")
-    #pisitools.doicon("atanks.png")
     pisitools.insinto("/usr/share/applications", "atanks.desktop")
     pisitools.insinto("/usr/share/pixmaps", "atanks.png")
 
