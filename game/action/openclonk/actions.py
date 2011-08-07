4045107f48ec1eb17bc34e54668e843c4f71b385
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-08-07 13:48:43 +0000

    add openclonk

---
+++ game/action/openclonk/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "openclonk"
+
+def setup():
+    cmaketools.configure()
+
+def build():
+    cmaketools.make()
+
+def install():
+    pisitools.dobin("clonk", "/opt/openclonk")
+    pisitools.insinto("/opt/openclonk", "planet/*")
+    pisitools.remove("/opt/openclonk/AUTHORS")
+    pisitools.remove("/opt/openclonk/COPYING")
+    pisitools.insinto("/usr/share/applications", "clonk.desktop")
+    pisitools.insinto("/usr/share/pixmaps", "planet/Graphics.ocg/Player.png", "clonk.png")
+
+    pisitools.dodoc("Credits.txt", "README.linux.txt", "licenses/*", "planet/AUTHORS", "planet/COPYING")
