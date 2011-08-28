7f588dc8c96b8b423d7e64788294438be346ea18
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-08-28 12:14:33 +0000

    ogre-pagedgeometry: version bump

--- trunk/library/ogre-pagedgeometry/actions.py
+++ trunk/library/ogre-pagedgeometry/actions.py
@@ -8,13 +8,15 @@ from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "ogre-pagedgeometry"
+WorkDir = "ogre-paged-%s" % get.srcVERSION()
 
 def setup():
+    pisitools.dosed("CMakeLists.txt", "} DESTINATION doc", "} DESTINATION share/doc/ogre-pagedgeometry")
+
     cmaketools.configure("-DPAGEDGEOMETRY_BUILD_SAMPLES=0")
 
 def build():
-    cmaketools.make()
+    cmaketools.make("--silent")
 
 def install():
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
