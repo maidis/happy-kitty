15956f5c80eb2b9a9062852818d3929fdfd95a46
safaariman@gmail.com <safaariman@gmail.com@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-10-21 22:21:51 +0000

    secondlife: edit build files

--- game/simulation/secondlife/actions.py
+++ game/simulation/secondlife/actions.py
@@ -11,9 +11,9 @@ WorkDir = "SecondLife-i686-%s" % get.srcVERSION()
 
 def install():
     for secondlife in ["app_settings", "bin", "character", "etc", "fonts", "lib", "res-sdl", "skins"]:
-        pisitools.insinto("/usr/share/SecondLife", secondlife)
+        pisitools.insinto("/opt/secondlife", secondlife)
     pisitools.insinto("/usr/share/pixmaps", "secondlife_icon.png", "secondlife.png")
-    pisitools.insinto("/usr/share/SecondLife", "secondlife")
-    pisitools.insinto("/usr/share/SecondLife", "gpu_table.txt")
+    pisitools.insinto("/opt/secondlife", "secondlife")
+    pisitools.insinto("/opt/secondlife", "gpu_table.txt")
     pisitools.dodoc("README-linux.txt", "README-linux-voice.txt", "licenses.txt", "README-linux-joystick.txt")
     
