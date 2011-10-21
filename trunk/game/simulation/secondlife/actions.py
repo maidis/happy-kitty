da95335185f0acc9626875b932613b125fa411c2
safaariman@gmail.com <safaariman@gmail.com@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-10-21 20:19:51 +0000

    secondlife: fix actions.py

--- trunk/game/simulation/secondlife/actions.py
+++ trunk/game/simulation/secondlife/actions.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
@@ -12,23 +12,8 @@ WorkDir = "SecondLife-i686-%s" % get.srcVERSION()
 def install():
     for secondlife in ["app_settings", "bin", "character", "etc", "fonts", "lib", "res-sdl", "skins"]:
         pisitools.insinto("/usr/share/SecondLife", secondlife)
-
     pisitools.insinto("/usr/share/pixmaps", "secondlife_icon.png", "secondlife.png")
-
-    pisitools.insinto("/usr/share/SecondLife", "secondlife", "secondlife")
-
-    pisitools.insinto("/usr/share/SecondLife", "featuretable_linux.txt", "featuretable_linux.txt")
-
-    pisitools.insinto("/usr/share/SecondLife", "gpu_table.txt", "gpu_table.txt")
-
-    pisitools.insinto("/usr/share/SecondLife", "licenses.txt", "licenses.txt")
-
-    pisitools.insinto("/usr/share/SecondLife", "README-linux-joystick.txt", "README-linux-joystick.txt")
-
-    pisitools.insinto("/usr/share/SecondLife", "README-linux-voice.txt", "README-linux-voice.txt")
-
-    pisitools.insinto("/usr/share/SecondLife", "README-linux.txt", "README-linux.txt")
-
-    pisitools.insinto("/usr/share/SecondLife", "summary.json", "summary.json")
-
+    pisitools.insinto("/usr/share/SecondLife", "secondlife")
+    pisitools.insinto("/usr/share/SecondLife", "gpu_table.txt")
     pisitools.dodoc("README-linux.txt", "README-linux-voice.txt", "licenses.txt", "README-linux-joystick.txt")
+    
