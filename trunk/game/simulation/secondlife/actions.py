a21d1795147b4d1fd5f5ded86956ffd5bad2ee37
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-10-25 15:53:13 +0000

    secondlife: not yet
    Update issue 68
    
    this is better but still not works

--- trunk/game/simulation/secondlife/actions.py
+++ trunk/game/simulation/secondlife/actions.py
@@ -12,8 +12,9 @@ WorkDir = "SecondLife-i686-%s" % get.srcVERSION()
 def install():
     for secondlife in ["app_settings", "bin", "character", "etc", "fonts", "lib", "res-sdl", "skins"]:
         pisitools.insinto("/opt/secondlife", secondlife)
+
     pisitools.insinto("/usr/share/pixmaps", "secondlife_icon.png", "secondlife.png")
     pisitools.insinto("/opt/secondlife", "secondlife")
     pisitools.insinto("/opt/secondlife", "gpu_table.txt")
+
     pisitools.dodoc("README-linux.txt", "README-linux-voice.txt", "licenses.txt", "README-linux-joystick.txt")
-    
--- trunk/game/simulation/secondlife/comar/package.py
+++ trunk/game/simulation/secondlife/comar/package.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 
 import os
-
-packages = "freealut-32bit gtk2-32bit libidn-32bit mesa-32bit libsdl-32bit alsa-lib-32bit pulseaudio-libs-32bit"
+import platform
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    os.system("hav call pisi System.Manager installPackage %s" % packages)
+    if platform.machine() == "x86_64":
+        for packages in ["freealut-32bit gtk2-32bit libidn-32bit mesa-32bit libsdl-32bit alsa-lib-32bit pulseaudio-libs-32bit"]:
+            os.system("hav call pisi System.Manager installPackage %s" % packages)
+
+    pass
 
 def preRemove():
     # FIXME This is not needed when upgrading package; but pisi does not
     #       provide a way to learn operation type.
-    #os.system("/usr/sbin/alternatives --remove libGL %s/libGL.so.1.2" % libdir)
     pass
