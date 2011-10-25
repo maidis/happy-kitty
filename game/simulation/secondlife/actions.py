41156d6c1af26c312959a3f1e4d0ab1199baf9db
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-10-25 19:13:55 +0000

    secondlife: it seems ok
    Update issue 68
    
    it's ok i guess but quite dirty
    
    please test it and if we won't any problem then we could close this review report :)

--- game/simulation/secondlife/actions.py
+++ game/simulation/secondlife/actions.py
@@ -4,6 +4,7 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
@@ -16,5 +17,15 @@ def install():
     pisitools.insinto("/usr/share/pixmaps", "secondlife_icon.png", "secondlife.png")
     pisitools.insinto("/opt/secondlife", "secondlife")
     pisitools.insinto("/opt/secondlife", "gpu_table.txt")
+
+    pisitools.dobin("secondlife-foo")
+
+    if get.ARCH() == "x86_64":
+        pisitools.domove("/usr/bin/secondlife-foo", "/usr/bin/32", "secondlife")
+        pisitools.dosed("secondlife.desktop", "Exec=secondlife", "Exec=/usr/bin/32/secondlife")
+    else:
+        pisitools.domove("/usr/bin/secondlife-foo", "/usr/bin", "secondlife")
+
+    pisitools.insinto("/usr/share/applications", "secondlife.desktop")
 
     pisitools.dodoc("README-linux.txt", "README-linux-voice.txt", "licenses.txt", "README-linux-joystick.txt")
