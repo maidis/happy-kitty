c3d2db4c0289403be92c7b612676cafd2d04531a
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-11-11 14:11:43 +0000

    add friking-shark

---
+++ trunk/game/arcade/friking-shark/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    # paths and png patches need more love
+    cmaketools.configure()
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    shelltools.chmod("%s/usr/bin/frikingshark" % get.installDIR(), 0755)
+
+    pisitools.dodoc("License.txt", "Readme.txt")
