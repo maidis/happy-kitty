de96df3e98801a01602baa3e102243cc28ea6b72
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2011-12-21 14:29:28 +0000

    add opengoo

---
+++ trunk/game/puzzle/opengoo/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import qt4
+
+def setup():
+    qt4.configure()
+
+def build():
+    qt4.make()
+
+def install():
+    pisitools.dobin("OpenGOO")
+
+    pisitools.rename("/usr/bin/OpenGOO", "opengoo")
+
+    pisitools.insinto("/usr/share/opengoo", "*.level")
+
+    pisitools.dodoc("LICENSE", "README")
