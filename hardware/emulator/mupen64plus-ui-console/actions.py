9fa88cbd1d4a58fbe7277cddf14e21328b4989b3
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2012-01-08 12:26:52 +0000

    add mupen64plus-ui-console

---
+++ hardware/emulator/mupen64plus-ui-console/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def build():
+    autotools.make("-C projects/unix all")
+
+def install():
+    autotools.rawInstall("-C projects/unix PREFIX=/usr MANDIR=/usr/share/man/man6/ DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("LICENSES", "README", "RELEASE")
