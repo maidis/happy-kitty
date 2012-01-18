aa4c2849bb46215926f50826d9943955174f4507
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2012-01-18 16:20:05 +0000

    add rigsofrods

---
+++ game/simulation/rigsofrods/actions.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "ror-2311"
+
+def setup():
+    cmaketools.configure("-DROR_BUILD_CONFIGURATOR=TRUE \
+                          -DROR_BUILD_CONVERTER=TRUE \
+                          -DROR_BUILD_SIM=TRUE \
+                          -DROR_BUILD_TOOLS=TRUE \
+                          -DROR_USE_MYGUI=TRUE \
+                          -DROR_USE_OPENAL=TRUE \
+                          -DROR_USE_SOCKETW=TRUE \
+                          -DROR_USE_PAGED=TRUE \
+                          -DROR_USE_CAELUM=TRUE \
+                          -DROR_USE_ANGELSCRIPT=TRUE \
+                          -DROR_USE_CURL=TRUE \
+                          -DSOCKETW_INCLUDE_DIRS=/usr/include \
+                          -DSOCKETW_LIBRARIES=/usr/lib/libSocketW.so \
+                          -DCMAKE_PREFIX_PATH=/opt/rigsofrods")
+
+def build():
+    cmaketools.make()
+
+def install():
+    #not works for now
+    #cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.insinto("/opt/rigsofrods/resources", "bin/resources/*")
+
+    pisitools.dobin("bin/RoR", "/opt/rigsofrods")
+    pisitools.dobin("bin/rorconfig", "/opt/rigsofrods")
+    pisitools.dobin("bin/tdr2svg", "/opt/rigsofrods")
+
+    pisitools.dolib_a("bin/libangelscript_addons.a", "/opt/rigsofrods")
+
+    #pisitools.insinto("/opt/rigsofrods", "bin/plugins.cfg")
+
+    pisitools.dodoc("COPYING", "LICENSE.txt", "readme.txt")
