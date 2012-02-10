91026a37f56155c0eee85af75f3a4ed5c773622d
ozbekanil <ozbekanil@6cc6fb36-c311-1c20-3e9c-79438a0a4627>
2012-02-10 19:16:06 +0000

    sfml: sync with git

--- trunk/multimedia/misc/sfml/actions.py
+++ trunk/multimedia/misc/sfml/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "LaurentGomila-SFML-369f450"
+WorkDir = "LaurentGomila-SFML-e3d75f6"
 
 def setup():
     cmaketools.configure("-DBUILD_DOC=TRUE")
